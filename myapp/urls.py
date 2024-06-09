0afbcdd67d6e15d1ecbcb52b1e552e4d919a5d46
Paulo Peter <eduardopeter91@gmail.com>
2024-06-09 17:28:19 -0300

    Ex.: Testes Automatizados

---
+++ myapp/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.lista_livros, name='lista_livros'),
+    path('adicionar/', views.adicionar_livro, name='adicionar_livro'),
+    path('atualizar/<int:pk>/', views.atualizar_livro, name='atualizar_livro'),
+]
--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Livro
+from .forms import LivroForm
 
-# Create your views here.
+def lista_livros(request):
+    livros = Livro.objects.all()
+    return render(request, 'myapp/lista_livros.html', {'livros': livros})
+
+def adicionar_livro(request):
+    if request.method == 'POST':
+        form = LivroForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_livros')
+    else:
+        form = LivroForm()
+    return render(request, 'myapp/adicionar_livro.html', {'form': form})
+
+def atualizar_livro(request, pk):
+    livro = get_object_or_404(Livro, pk=pk)
+    if request.method == 'POST':
+        form = LivroForm(request.POST, instance=livro)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_livros')
+    else:
+        form = LivroForm(instance=livro)
+    return render(request, 'myapp/atualizar_livro.html', {'form': form, 'livro': livro})
